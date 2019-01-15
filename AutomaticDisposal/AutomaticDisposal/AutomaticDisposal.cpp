aace3b9ba99bfb0cb62551947d0d23696d52913c
skynt001 <skynt001@gmail.com>
2019-01-15 17:23:25 +0900

    modify AutomaticDisposal - DisposedFlag

--- AutomaticDisposal/AutomaticDisposal/AutomaticDisposal.cpp
+++ AutomaticDisposal/AutomaticDisposal/AutomaticDisposal.cpp
@@ -69,3 +69,87 @@ void FileDumper::Dump()
 	if (streamReader != nullptr)
 		Console::WriteLine(streamReader->ReadToEnd());
 }
+
+
+///////////////////////////////////////////////
+// DisposedFlag
+DisposedFlag::DisposedFlag(String^ objectName)
+	: isDisposed(false), objectName(objectName)
+{
+}
+
+DisposedFlag::~DisposedFlag()
+{
+	isDisposed = true;
+}
+
+DisposedFlag::!DisposedFlag()
+{
+
+}
+
+DisposedFlag::operator bool()
+{
+	return isDisposed;
+}
+
+void DisposedFlag::EnsureObjectIsNotDisposed()
+{
+	if (isDisposed)
+		throw gcnew ObjectDisposedException(objectName);
+}
+
+///////////////////////////////////////////////
+// SmartFileDumper
+SmartFileDumper::SmartFileDumper(String^ name)
+	: disposedFlag("FileDumper")
+{
+	fileStream = nullptr;
+	try
+	{
+		fileStream = gcnew FileStream(name, FileMode::Open);
+
+		streamReader = nullptr;
+		try
+		{
+			streamReader = gcnew StreamReader(fileStream);
+
+			// TODO:
+
+		}
+		catch (Exception^ e)
+		{
+			Console::WriteLine("Failed to read file with the following error: " + e->Message);
+
+			throw;
+		}
+		finally
+		{
+			streamReader->Close();
+		}
+	}
+	catch (Exception^ e)
+	{
+		Console::WriteLine("Failed to open file with the following error: " + e->Message);
+	}
+	finally
+	{
+		fileStream->Close();
+	}
+}
+
+void SmartFileDumper::Dump()
+{
+	disposedFlag.EnsureObjectIsNotDisposed();
+
+	if (streamReader != nullptr)
+		Console::WriteLine(streamReader->ReadToEnd());
+}
+
+void SmartFileDumper::CheckDisposed()
+{
+	if (disposedFlag)
+		Console::WriteLine("FileDumper is disposed");
+	else
+		Console::WriteLine("FileDumper is not disposed");
+}
--- AutomaticDisposal/AutomaticDisposal/AutomaticDisposal.h
+++ AutomaticDisposal/AutomaticDisposal/AutomaticDisposal.h
@@ -21,4 +21,39 @@ namespace AutomaticDisposal {
 		StreamReader^ streamReader;
 	};
 
+	///////////////////////////////////////////////
+	// DisposedFlag
+	ref class DisposedFlag
+	{
+	public:
+		explicit DisposedFlag(String^ objectName);
+		~DisposedFlag();
+		!DisposedFlag();
+
+		operator bool();
+
+		void EnsureObjectIsNotDisposed();
+
+	private:
+		bool isDisposed;
+		String^ objectName;
+	};
+
+	///////////////////////////////////////////////
+	// SmartFileDumper
+	public ref class SmartFileDumper
+	{
+	public:
+		explicit SmartFileDumper(String^ name);
+
+		void Dump();
+		void CheckDisposed();
+
+	private:
+		DisposedFlag disposedFlag;
+
+		FileStream^ fileStream;
+		StreamReader^ streamReader;
+	};
+
 }
